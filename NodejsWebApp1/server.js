321c903912a3fbae9add8a55ed196e0577f65b5f
Will Truscott <9992022@student.swin.edu.au>
2016-08-28 21:15:09 +1000

    ejs rendering now works

--- NodejsWebApp1/server.js
+++ NodejsWebApp1/server.js
@@ -2,6 +2,11 @@
 var http = require('http');
 var express = require('express');
 var mongoose = require('mongoose');
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+var request = require("request");
+require('longjohn');
+require("console-stamp")(console, { pattern: "dd/mm/yyyy HH:MM:ss.l" });
 
 // This function creates a UUID that is nigh impossible to occur twice. (This function was taken from a previous project of Will Truscott's (9992022))
 function makeId(){
@@ -31,14 +36,78 @@ function makeId(){
     });
 }
 
+global.rootdir = __dirname + "\\" + "\client";
+var rootdir = __dirname + "\\" + "\client";
+var app = express();
+
+app.use(cookieParser());
+app.engine('.html', require('ejs').renderFile)	//may not be necessary? http://expressjs.com/en/api.html#app.engine
+app.set("view engine", "ejs");
+app.set('views', global.rootdir);
+
+var prerouter = new express.Router()
+	//logging
+	.use(function (req, res, next) {
+    console.log("-------------");
+    console.log(req.method);
+    console.log(req.url);
+    console.log(req.path);
+    console.log("-------------");
+    next();
+})
+    //does some basic formatting - normalising the return from different body/cookie parsers and methods, etc.
+	.use(function (req, res, next) {
+    if (typeof req.body == "string") {	//seems under some cicumstances, the body is not parsed from JSON properly, so we have to do it again here. TODO: investigate.
+        try {
+            req.body = JSON.parse(req.body);
+        } catch (e) {
+				//probably just means it's a normal string instead of a JSON string
+        }
+    }
+    if (typeof req.body !== "undefined" && Object.keys(req.body).length > 0) {
+        req.query = req.body;
+    }
+    next();
+});
 
 
+var webserverrouter = new express.Router()
+.all('/', function (req, res) {
+    res.render("index.html", { query: req.query }, function (err, html) {
+        console.log(err);
+        if (err) {
+            next();
+        } else {
+            res.send(html);
+        }
+    });
+})
+//ejs templates
+.use('*.html', function (req, res, next) {
+    res.render(req.baseUrl.substring(1), { query: req.query }, function (err, html) {
+        if (err) {
+            next();
+        } else {
+            res.send(html);
+        }
+    });
+})
+//other static servings
+.use(express.static(rootdir))
+
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.raw());
+
+app.use("", prerouter);	//prerouter needs to come after bodyparser
+app.use("", webserverrouter);
 
 
-var port = process.env.port || 3001;
-console.log("Server running on port 3001");
+app.all(function (req, res) {
+    console.log("404 redirect...", { "root": rootdir });
+    res.redirect("/404.html");
+});
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+console.log("app running on port 2089");
+app.listen(2089);
