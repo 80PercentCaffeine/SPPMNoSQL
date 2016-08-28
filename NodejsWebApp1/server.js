8163842291758fae6ca60f19a53e72e0ab5b62fb
Will Truscott <9992022@student.swin.edu.au>
2016-08-28 18:27:32 +1000

    Initial commit. Project architecture.

---
+++ NodejsWebApp1/server.js
@@ -0,0 +1,9 @@
+﻿var http = require('http');
+
+var port = process.env.port || 3001;
+console.log("Server running on port 3001");
+
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World\n');
+}).listen(port);
