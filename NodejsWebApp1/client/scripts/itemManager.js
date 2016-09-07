3c69d693dd82610ba0ecd502550e07dee5d463af
Will Truscott <9992022@student.swin.edu.au>
2016-09-07 22:06:56 +1000

    Item adding

--- NodejsWebApp1/client/scripts/itemManager.js
+++ NodejsWebApp1/client/scripts/itemManager.js
@@ -1,10 +1,37 @@
+var items = [];
+
+// This function creates a UUID that is nigh impossible to occur twice. (This function was taken from a previous project of Will Truscott's (9992022))
+function makeId() {
+    var array = new Uint8Array(16);
+    var val = new Uint8Array(1);
+    crypto.getRandomValues(array);
+    var idx = 0;
+    var shouldincrement = false;
+    var uuidtemplate = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx";
+    var makeuuidregex = /[xy]/g;
+    
+    return uuidtemplate.replace(makeuuidregex,
+			function (c) {
+        if (c == 'x') {
+            if (!shouldincrement) {
+                val[0] = array[idx] >>> 4;
+            } else {
+                val[0] = array[idx] << 4;
+                val[0] = val[0] >>> 4;
+                idx++;
+            }
+            shouldincrement = !shouldincrement;
+            return val[0].toString(16);
+        } else {
+            return (0x8 + (array[idx] >>> 6)).toString(16);
+        }
+    });
+}
+
 function setupTable(docs){
-    docs = [
-        { "UUID": 1, "name": "hi",     "price": "$10" }, 
-        { "UUID": 2, "name": "hello",  "price": "$20" }, 
-        { "UUID": 3, "name": "hello2", "price": "$11" }
-    ];
+    items = docs;
     var itemTable = document.getElementById("itemDisplay");
+    itemTable.innerHTML = "";
     var tr = document.createElement("tr");
     var td = document.createElement("td");
     td.innerHTML = "Name";
@@ -33,11 +60,18 @@ function setupTable(docs){
 }
 
 function displayItem(UUID){
-    console.log(UUID);
+    for (var i = 0; i < items.length; i++) {
+        if (items[i].UUID == UUID) {
+            var toDisplay = items[i];
+            console.log(items[i].name);
+        }
+    }
 }
 
 function addItem() {
-    console.log(UUID);
+    console.log(makeId());
+    items.push({ "UUID": makeId(), "name": "", "price": "$0" });
+    setupTable(items);
 }
 
 function deleteItem(UUID) {
@@ -46,5 +80,9 @@ function deleteItem(UUID) {
 
 // TODO: replace with db call.
 function init(){
-	setupTable("");
+	setupTable([
+        { "UUID": makeId(), "name": "hi", "price": "$10" }, 
+        { "UUID": makeId(), "name": "hello", "price": "$20" }, 
+        { "UUID": makeId(), "name": "hello2", "price": "$11" }
+    ]);
 }
